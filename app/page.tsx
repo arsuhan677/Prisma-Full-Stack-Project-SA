8ba62acc4f86175d460a1126e0cf53cec5db91ea
AR Suhan <aminurrahmansuhan@gmail.com>
2026-01-27 00:48:00 +0600

    update for db

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,50 @@
+// import Header from '@/components/header/Header';
+// import Footer from '@/components/footer/Footer';
+// import Hero from '@/components/sections/Hero';
+// import Categories from '@/components/sections/Categories';
+// import BestDeals from '@/components/sections/BestDeals';
+// import HeroTwo from '@/components/sections/HeroTwo';
+// import JustForYou from '@/components/sections/JustForYou';
+// import Newsletter from '@/components/Newsletter';
+// import TrendingNow from '@/components/sections/TrendingNow';
+// import ServiceHighlights from '@/components/ServiceHighlights';
+// import prisma from '@/lib/prisma';
+// // import RelatedProduct from '@/components/shared/RelatedProduct';
+
+// export default async function Home() {
+//   const products = await prisma.product.findMany();
+//   // const categories = await prisma.category.findMany();
+//   return (
+//     <>
+//       <Header />
+
+//       <main>
+//         <Hero />
+
+//         <div className="space-y-[50px] md:space-y-[80px] lg:space-y-[120px] mt-[50px] md:mt-[80px] lg:mt-[120px]">
+//           <Categories />
+//           <BestDeals products={products} />
+//         </div>
+
+//         <div className="mt-[50px] md:mt-[80px] lg:mt-[120px]">
+//           <HeroTwo />
+//         </div>
+
+//         <div className="space-y-[50px] md:space-y-[80px] lg:space-y-[120px] mt-[50px] md:mt-[80px] lg:mt-[120px]">
+//           <JustForYou products={products} />
+//           <Newsletter />
+//           <TrendingNow products={products} />
+//           {/* <RelatedProduct /> */}
+//           <ServiceHighlights />
+//         </div>
+//       </main>
+
+//       <Footer />
+//     </>
+//   );
+// }
+
+
 import Header from '@/components/header/Header';
 import Footer from '@/components/footer/Footer';
 import Hero from '@/components/sections/Hero';
@@ -8,12 +55,17 @@ import JustForYou from '@/components/sections/JustForYou';
 import Newsletter from '@/components/Newsletter';
 import TrendingNow from '@/components/sections/TrendingNow';
 import ServiceHighlights from '@/components/ServiceHighlights';
-import prisma from '@/lib/prisma';
-// import RelatedProduct from '@/components/shared/RelatedProduct';
+
+async function getProducts() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`, {
+    cache: "no-store",
+  });
+  return res.json();
+}
 
 export default async function Home() {
-  const products = await prisma.product.findMany();
-  // const categories = await prisma.category.findMany();
+  const products = await getProducts();
+
   return (
     <>
       <Header />
@@ -34,7 +86,6 @@ export default async function Home() {
           <JustForYou products={products} />
           <Newsletter />
           <TrendingNow products={products} />
-          {/* <RelatedProduct /> */}
           <ServiceHighlights />
         </div>
       </main>
@@ -43,3 +94,4 @@ export default async function Home() {
     </>
   );
 }
+
